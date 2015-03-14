1ac745d5b60f85501cebde466b837f487bac5f20
Pavel Razuvayev <pavel.razuvayev@gmail.com>
2015-03-14 18:34:27 -0500

    THFunFacts

---
+++ app/src/main/java/info/ferz/funfacts/FunFactsActivity.java
@@ -0,0 +1,57 @@
+package info.ferz.funfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+
+public class FunFactsActivity extends ActionBarActivity {
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables and assign the Views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                // Update the label with our dynamic fact
+                factLabel.setText(fact);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+
+        showFactButton.setOnClickListener(listener);
+
+        // Long way
+        // String message = "Yay! Our Activity was created";
+        // Toast welcomeToast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        // welcomeToast.show();
+
+        // Method chaining, the same as above
+        // Toast.makeText(this, "Yay! Our Activity was created!", Toast.LENGTH_LONG).show();
+
+    }
+}
